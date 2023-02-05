8daca39470be14ab50dd7473cc6adf90c07f6d36
kirad26 <105429600+kirad26@users.noreply.github.com>
2023-02-05 18:17:26 +0200

    правки

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -10,13 +10,10 @@ public class Water : MonoBehaviour
     private BoxCollider2D m_Sprite; // отвечает за работу нашего слайна с водой
     public int waterValue = 5; // количество воды, которое добавляем при касании
 
-    void Start()
-    {
-        //tmpText = GameObject.FindGameObjectWithTag("Sunbeam").GetComponent<TextMeshProUGUI>(); // берем со сцены наше значение ресурса
+    private void OnTriggerEnter2D(Collider2D other) { 
         tmpObject = GameObject.FindGameObjectWithTag("Roots").GetComponent<BackScript>(); // ищем наш скрипт на сцене
         m_Sprite = GetComponent<BoxCollider2D>(); // берем значение спрайта
-    }
-    private void OnTriggerEnter2D(Collider2D other) { // функция работы с триггером
+        // функция работы с триггером
         if(other.tag == "Roots") // проверка, что за триггер сработал
         {
             m_Sprite.isTrigger = false; // блокируем, чтобы не было множественное срабатывание
